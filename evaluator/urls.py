48d041681cbee2440ae6a47c060b4e1916624c42
German Blanco <german.blanco@gmail.com>
2021-11-04 16:23:15 +0100

    addroot

--- evaluator/urls.py
+++ evaluator/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponse
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('', lambda request: HttpResponse('the cow jumped over the moon')),
 ]
