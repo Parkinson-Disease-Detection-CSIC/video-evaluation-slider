863ce4de9983ffc9584dc1c3ad1e4f73a65572f1
German Blanco <german.blanco@gmail.com>
2021-11-04 08:24:15 +0100

    draft

--- evaluator/settings.py
+++ evaluator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 import django_heroku
 
 
@@ -70,11 +71,8 @@ WSGI_APPLICATION = "evaluator.wsgi.application"
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
