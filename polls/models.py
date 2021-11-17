fb6f255dc4ed6debb6baa5f2ae12b8f76b3c6689
German Blanco <german.blanco@gmail.com>
2021-11-17 15:49:32 +0100

    addtimespent

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 class Evaluation(models.Model):
-    evaluator = models.CharField(max_length=40)
+    evaluator = models.CharField(db_index=True, max_length=40)
     date = models.DateTimeField('evaluation date')
-    subject = models.IntegerField()
-    exercise = models.CharField(max_length=40)
+    subject = models.IntegerField(db_index=True)
+    time_spent_general = models.IntegerField()
+    time_spent_symptoms = models.IntegerField()
     tremor = models.FloatField()
     bradykinesia = models.FloatField()
     stability = models.FloatField()
