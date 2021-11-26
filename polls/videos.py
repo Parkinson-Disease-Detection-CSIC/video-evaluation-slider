a8dbd00903b5de013fc888e3665ad9064645bf19
German Blanco <german.blanco@gmail.com>
2021-11-26 18:10:14 +0100

    movevideostocsv

--- polls/videos.py
+++ polls/videos.py
@@ -1,10 +1,27 @@
+import csv
+from functools import cache
 
-video_urls = {
-  39: {
-    "all": "https://drive.google.com/file/d/1cV40Ufac3H8yQ6dEOxf2tjL7O9CyYCJC/preview",
-    "meters15": "https://drive.google.com/file/d/1YNyKrfF0-BeXWxGSlrVqIyGxh96x5sR9/preview",
-  },
-  41: {
-    "meters15": "https://drive.google.com/file/d/1sHM167RTxsQ1ifUb-NX1qiKlJ48ct41z/preview",
-  },
-}
+def get_url_from_id(video_id):
+    return "https://drive.google.com/file/d/" + video_id + "/preview"
+
+@cache
+def get_video_urls():
+    video_urls = {}
+    with open('videos.csv') as videos_csv:
+        csv_reader = csv.reader(videos_csv, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                subject = int(row[0])
+                exercise = row[1]
+                video_id = row[2]
+
+                if subject in video_urls:
+                    videos_dict = video_urls[subject]
+                else:
+                    videos_dict = {}
+                videos_dict[exercise] = get_url_from_id(video_id)
+                video_urls[subject] = videos_dict
+    return video_urls
